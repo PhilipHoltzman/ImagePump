9d0463043e1d751fe89aa7d1d7a7c6412028cc3b
Philip Holtzman <phil.holtzman@gmail.com>
2018-12-16 21:24:37 -0500

    initial commit

---
+++ conversion.py
@@ -0,0 +1,13 @@
+# basic script to convert png to jpeg
+
+from PIL import Image
+import sys
+import os
+
+path2png = "/Users/poisonarena/Desktop/jpg"
+path2jpg = "path"
+
+
+os.chdir(path2png)
+
+print(os.getcwd())
